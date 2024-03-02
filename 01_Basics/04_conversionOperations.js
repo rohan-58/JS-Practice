c40abd1fd607d13984367ab0597a02b18ccd1463
ROHAN PRAMANIK <121264513+rohan-58@users.noreply.github.com>
2024-03-02 07:49:02 +0000

    convertion

---
+++ 01_Basics/04_conversionOperations.js
@@ -0,0 +1,27 @@
+/***************************************OPERATIONS******************************************************/
+let val = 3
+let negVal = -val
+console.log(negVal)  //   -3
+
+console.log(2+2)  //addition                4
+console.log(2-2)   //substruction           0
+console.log(2*5)   //multiplication         10    
+console.log(2**4)   //power                 16
+console.log(2/2)   //division               1
+console.log(3%2)   //remainder              1
+
+//string concatinate
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1+"2")   //integer + string ---> 12
+console.log("2"+"1")   //string + string ---> 21
+console.log("2"+3+3)   //233
+console.log(2+3+"3")   //53
+console.log(2+"3"+3)   //233
+
+console.log(true)   //o/p --> true
+console.log(+true)    //o/p --> 1   here we have increment the boolean operator
+console.log(+"")     //op ---> 0
