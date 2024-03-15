a5896b26efe269beff6dca0cfb9969df5caeeb0b
ROHAN PRAMANIK <121264513+rohan-58@users.noreply.github.com>
2024-03-16 00:13:41 +0530

    Delete 06_Map.js

--- 06_Map.js
+++
@@ -1,9 +0,0 @@
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-//const numAddTen = nums.map((num) => num+10)     // call back function without name
-//const numAddTen = nums.map((num) => {return num+10})     // call back function without name
-
-// Here we can perform chaining (menas we can preform multiple method)
-const numAddTen = nums.map((num) => num*10).map((num) => num+1).filter((num) => num>=40);
-
-console.log(numAddTen)      //map automatically return value
